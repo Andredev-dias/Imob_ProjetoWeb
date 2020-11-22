9ac44526e873eeffdb2c9a33f5f1b2f47bd6f9c9
Andre Dias <andredias.curitiba@gmail.com>
2020-11-22 20:44:10 -0300

    Criando repositorio e views locatario parcial

---
+++ Imob/Controllers/EntrarController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Imob.Controllers
+{
+    public class EntrarController : Controller
+    {
+        public IActionResult Index() => View();
+
+       public IActionResult Entrar()
+        {
+            return RedirectToAction("InicialMenu", "Home");
+        }
+    }
+}
---
+++ Imob/Controllers/LocatarioController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Imob.Controllers
+{
+    public class LocatarioController : Controller
+    {
+        public IActionResult CadastrarLocatario()
+        {
+            return View();
+        }
+        public IActionResult ListaLocatarios()
+        {
+            return View();
+        }
+    }
+}
