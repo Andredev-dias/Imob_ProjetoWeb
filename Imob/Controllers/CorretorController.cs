90422fc770cb3c43063a605e3a14fb1ba2d167c1
Andre Dias <andredias.curitiba@gmail.com>
2020-11-25 18:40:38 -0300

    Criação COntrollers Corretor/CSS ajustes/ Criação views Corretor inicial

---
+++ Imob/Controllers/CorretorController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Imob.Controllers
+{
+    public class CorretorController : Controller
+    {
+        public IActionResult CadastrarCorretor()
+        {
+            return View();
+        }
+
+
+        public IActionResult ListaCorretores()
+        {
+            return View();
+        }
+    }
+}
