d85541eec7440a512de4ecab99f563dcd1255e6d
Andrey Suprano <andsuprano@gmail.com>
2020-11-30 01:45:41 -0300

    Implementa Cadastro de Imóveis

--- Imob/DAL/TipoImovelDAO.cs
+++ Imob/DAL/TipoImovelDAO.cs
@@ -1,13 +1,15 @@
 ﻿using Imob.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Imob.DAL
 {
-    class TipoImovelDAO
+    public class TipoImovelDAO
     {
         private readonly Context _context;
 
         public TipoImovelDAO(Context context) => _context = context;
-
+        public List<TipoImovel> Listar() => _context.TipoImovel.ToList();
         public TipoImovel BuscarPorId(int id) =>
             _context.TipoImovel.Find(id);
     }
